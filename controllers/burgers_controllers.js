eab702467d9e8824500586a295c88be26cb0a557
Armando Contreras <armandocontreras62596@gmail.com>
2021-03-07 19:50:51 -0800

    fixed my put request code

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -29,7 +29,7 @@ router.put("/api/burgers/:id", function (req, res) {
 
     console.log("condition", condition);
 
-    burgers.update({
+    burger.updateOne({
         devoured: req.body.devoured
     }, condition, function (result) {
         if (result.changedRows == 0) {
@@ -41,4 +41,4 @@ router.put("/api/burgers/:id", function (req, res) {
     });
 });
 
-module.exports = router;4
+module.exports = router;
